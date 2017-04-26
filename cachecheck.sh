1cab7afeb3bee1c5732cdc158e68fe65d445cbc5
AP Orlebeke <aporlebeke@gmail.com>
2017-04-26 17:13:22 -0400

    Add check for macOS version 12

--- cachecheck.sh
+++ cachecheck.sh
@@ -6,5 +6,12 @@
 #Note that the return is either a multi-line output of IPs or null if none are found
 #
 #
-result=`/usr/bin/AssetCacheLocatorUtil 2>&1 | grep guid | awk '{print$4}' | sed 's/^\(.*\):.*$/\1/' | uniq`
+osvers=$(sw_vers -productVersion | awk -F. '{print $2}')
+
+if [ "$osvers" < "12" ]; then
+  result=""
+else
+  result=`/usr/bin/AssetCacheLocatorUtil 2>&1 | grep guid | awk '{print$4}' | sed 's/^\(.*\):.*$/\1/' | uniq`
+fi
+
 echo "<result>$result</result>"
