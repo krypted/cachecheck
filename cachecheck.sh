069b8f5812ad1ad38825f4ebe16a9b433bab8915
Aaron Lippincott <psychokiller_2002@yahoo.com.au>
2019-07-25 08:46:39 +1000

    add: sort before uniq
    uniq requires input which is sorted first, this fixes the bug where multiple lines of the same IP address are returned

--- cachecheck.sh
+++ cachecheck.sh
@@ -6,5 +6,5 @@
 #Note that the return is either a multi-line output of IPs or null if none are found
 #
 #
-result=`/usr/bin/AssetCacheLocatorUtil 2>&1 | grep guid | awk '{print$4}' | sed 's/^\(.*\):.*$/\1/' | uniq`
+result=`/usr/bin/AssetCacheLocatorUtil 2>&1 | grep guid | awk '{print$4}' | sed 's/^\(.*\):.*$/\1/'| sort | uniq`
 echo "<result>$result</result>"
