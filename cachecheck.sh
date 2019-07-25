57353e1d2245b0ace8039824511310647d05081b
Charles Edge <krypted@me.com>
2019-07-25 08:37:50 -0500

    Update cachecheck.sh

--- cachecheck.sh
+++ cachecheck.sh
@@ -2,9 +2,11 @@
 #
 #
 #
-#Jamf Pro Extension Attribute to return the active Caching Server(s) found by a Mac
+#Jamf Pro Extension Attribute used to return the active Caching Server(s) found by a Mac
 #Note that the return is either a multi-line output of IPs or null if none are found
 #Note - each server is listed once whether it caches iCloud data or just shared assets
 #
+#
+$
 result=`/usr/bin/AssetCacheLocatorUtil 2>&1 | grep guid | awk '{print$4}' | sed 's/^\(.*\):.*$/\1/' | sort | uniq`
 echo "<result>$result</result>"
